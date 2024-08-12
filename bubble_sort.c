ddd17f22e35ed6a851e95277e32d0c5dfe2e18e2
zhumouren <zhumouren0623@qq.com>
2024-08-12 22:04:51 +0800

    add bubble_sort

---
+++ bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void bubble_sort(int list[], int len)
+{
+    int tmp;
+    for (int i = 0; i < len - 1; ++i) {
+        for (int j = 0; j < len - i - 1; ++j) {
+            if (list[j] > list[j+1]) {
+                tmp = list[j];
+                list[j] = list[j+1];
+                list[j+1] = tmp;
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+//    int list[] = {1, 4, 2, 5, 8, 0 ,9, 3, 6, 7};
+    int list[] = {22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70};
+    bubble_sort(list, 14);
+    for (int i = 0; i < 14; ++i) {
+        printf("%d\t", list[i]);
+    }
+    printf("\n");
+
+
+    return 0;
+}
