a10937e22a9579fa8fc43b7d61a464e8c9784f99
zhumouren <zhumouren0623@qq.com>
2024-08-12 22:10:26 +0800

    bubble_sort 使用sizeof判断len

--- bubble_sort.c
+++ bubble_sort.c
@@ -18,8 +18,9 @@ int main(int argc, char *argv[])
 {
 //    int list[] = {1, 4, 2, 5, 8, 0 ,9, 3, 6, 7};
     int list[] = {22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70};
-    bubble_sort(list, 14);
-    for (int i = 0; i < 14; ++i) {
+    int len = sizeof(list) / sizeof(list[0]);
+    bubble_sort(list, len);
+    for (int i = 0; i < len; ++i) {
         printf("%d\t", list[i]);
     }
     printf("\n");
